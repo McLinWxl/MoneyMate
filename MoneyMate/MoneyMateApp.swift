e76aaac2ab42bc6ebf32697a7adeba003c247b19
Xinlin Wang <wangxinlin525@gmail.com>
2025-07-16 12:44:48 +0800

    Initial Commit

---
+++ MoneyMate/MoneyMateApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoneyMateApp.swift
+//  MoneyMate
+//
+//  Created by McLin on 2025/7/16.
+//
+
+import SwiftUI
+
+@main
+struct MoneyMateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
